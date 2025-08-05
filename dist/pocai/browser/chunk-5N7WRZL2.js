34859aac58f051c323595eade84494481f00ce45
nrmmsathish <satheric1234@gmail.com>
2025-08-05 21:03:21 +0530

    updates to dashboard and lead management

---
+++ dist/pocai/browser/chunk-5N7WRZL2.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e}from"./chunk-EB7STPBV.js";import"./chunk-KKG22X4T.js";export{e as FunnelChart,d as FunnelController,a as TrapezoidElement,c as blues,b as pickForegroundColorToBackgroundColor};
