a1e459df44a4072254a9fcb759eece618bb60205
nrmmsathish <satheric1234@gmail.com>
2025-08-06 11:00:19 +0530

    Update fixes for lead management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
-import { Routes } from '@angular/router';
+
 import { LeadsManagementDashboard } from './leads-management-dashboard/leads-management-dashboard';
 import { Dashboard } from './dashboard/dashboard';
+import { Routes, provideRouter, withHashLocation } from '@angular/router';
 
 export const routes: Routes = [
     { path: 'leads', component: LeadsManagementDashboard },
     { path: 'advisor', component: Dashboard },
     { path: '', redirectTo: 'advisor', pathMatch: 'full' } // Set default route to advisor
+];
+
+export const routerProviders = [
+  provideRouter(routes, withHashLocation())
 ];
